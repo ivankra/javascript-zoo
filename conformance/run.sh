d3abb7cdc9a761caa8a9914e43840751e51b9d36
Ivan Krasilnikov <ivank@ivank.me>
2026-02-24 04:11:40 +0800

    see

--- conformance/run.sh
+++ conformance/run.sh
@@ -92,7 +92,7 @@ case "$ENGINE_NAME" in
     ENGINE_CMD+=(--script);;
   escargot|jerryscript|jsc|nashorn|xs|cesanta-v7|rpython-langjs|topchetoeu)
     ENGINE_CMD+=("$SCRIPT_DIR/var-console-log.js");;
-  hermes|mocha|spidermonkey_[12]*|carakan|kjs|ejscript|malbrain|ngs|starlight|yrm006)
+  hermes|mocha|spidermonkey_[12]*|carakan|kjs|ejscript|malbrain|ngs|see|starlight|yrm006)
     ENGINE_CMD=("$SCRIPT_DIR/sed-console-log.sh" "${ENGINE_CMD[@]}");;
   nova)
     ENGINE_CMD=("$SCRIPT_DIR/sed-console-log.sh" "${ENGINE_CMD[@]}" eval);;
