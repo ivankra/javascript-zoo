781cc93956bd6ac6a78ea2c46655bc9e865d2623
Ivan Krasilnikov <ivank@ivank.me>
2026-03-28 19:40:11 +0800

    harness: improve config

--- engines/hermes/prelude.js
+++ engines/hermes/prelude.js
@@ -1,9 +1,19 @@
-// Prelude for test262
-
 // hermes-v1 has a built-in partial $262:
 // https://github.com/facebook/hermes/blob/static_h/tools/shermes/ConsoleBindings.cpp
 
-// Add gc() method to it if available
-if (typeof $262 === 'object' && typeof $262.gc === 'undefined' && typeof globalThis.gc === 'function') {
+// hermes main doesn't define console.log(), only print().
+if (typeof console === 'undefined' && typeof print !== 'undefined') {
+  globalThis.console = {log: print};
+}
+
+if (typeof $262 === 'undefined') {
+  globalThis.$262 = {};
+}
+
+if (typeof $262.eval === 'undefined') {
+  $262.eval = function (s) { return (0, eval)(s); }
+}
+
+if (typeof $262.gc === 'undefined' && typeof globalThis.gc === 'function') {
   $262.gc = globalThis.gc;
 }
